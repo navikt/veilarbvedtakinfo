e9ea733c631319a03a06f15c26147766d1fc5322
Izis Frøyhov <izis.froyhov@nav.no>
2019-04-09 11:34:34 +0200

    KAIZEN-0: Fikse oppsett

---
+++ src/test/java/MainTest.java
@@ -0,0 +1,16 @@
+import no.nav.testconfig.ApiAppTest;
+
+import static no.nav.fo.veilarbvedtakinfo.config.ApplicationConfig.APPLICATION_NAME;
+
+public class MainTest {
+    private static final String PORT = "8800";
+
+    public static void main(String[] args) throws Exception {
+        ApiAppTest.setupTestContext(ApiAppTest.Config.builder()
+                .applicationName(APPLICATION_NAME)
+                .build()
+        );
+
+        Main.main(PORT);
+    }
+}
