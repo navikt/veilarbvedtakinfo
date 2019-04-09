33781331668b6d7a67378b9a8aafc1e00f8178cc
Izis Frøyhov <izis.froyhov@nav.no>
2019-04-09 15:06:27 +0200

    KAIZEN-0: Forenkle oppsett og oppgradere common

--- src/main/java/no/nav/fo/veilarbvedtakinfo/Main.java
+++ src/main/java/no/nav/fo/veilarbvedtakinfo/Main.java
@@ -1,14 +1,10 @@
 package no.nav.fo.veilarbvedtakinfo;
 import no.nav.apiapp.ApiApp;
 import no.nav.fo.veilarbvedtakinfo.config.ApplicationConfig;
-import static java.lang.System.setProperty;
-import static no.nav.fo.veilarbvedtakinfo.config.ApplicationConfig.APPLICATION_NAME;
 
 public class Main {
 
     public static void main(String... args) throws Exception {
-        setProperty("applicationName", APPLICATION_NAME);
-
         ApiApp.runApp(ApplicationConfig.class, args);
     }
 
