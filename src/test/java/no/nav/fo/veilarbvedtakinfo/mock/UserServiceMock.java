b1c230d4aebfd129ab6429739c4f95d1a00c806b
Izis Frøyhov <izis.froyhov@nav.no>
2019-04-12 10:15:27 +0200

    FO-2172: Userservice

---
+++ src/test/java/no/nav/fo/veilarbvedtakinfo/mock/UserServiceMock.java
@@ -0,0 +1,21 @@
+package no.nav.fo.veilarbvedtakinfo.mock;
+
+import no.nav.fo.veilarbvedtakinfo.service.UserService;
+
+import javax.inject.Provider;
+import javax.servlet.http.HttpServletRequest;
+
+public class UserServiceMock extends UserService {
+
+    public UserServiceMock(Provider<HttpServletRequest> requestProvider) {
+        super(requestProvider);
+    }
+
+    public boolean erEksternBruker() {
+        return true;
+    }
+
+    public String getUid() {
+        return "12345678910"; //TODO: Fjerne fnr
+    }
+}
