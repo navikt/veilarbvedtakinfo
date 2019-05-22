4980fc91a9a4e1b64367a3478c7bd2029e4aebe8
Izis Frøyhov <izis.froyhov@nav.no>
2019-05-22 14:19:32 +0200

    FO-2041: Rette feil

--- src/main/java/no/nav/fo/veilarbvedtakinfo/domain/infoommeg/HelseOgAndreHensynData.java
+++ src/main/java/no/nav/fo/veilarbvedtakinfo/domain/infoommeg/HelseOgAndreHensynData.java
@@ -2,6 +2,7 @@ package no.nav.fo.veilarbvedtakinfo.domain.infoommeg;
 
 import lombok.Data;
 import lombok.experimental.Accessors;
+import no.nav.fo.veilarbvedtakinfo.domain.registrering.AndreForholdSvar;
 import no.nav.fo.veilarbvedtakinfo.domain.registrering.HelseHinderSvar;
 
 import java.util.Date;
@@ -11,4 +12,13 @@ import java.util.Date;
 public class HelseOgAndreHensynData extends DataItem {
     HelseHinderSvar verdi;
     Date dato;
+
+    public HelseOgAndreHensynData setVerdi(AndreForholdSvar andreForholdSvar) {
+        verdi = HelseHinderSvar.valueOf(andreForholdSvar.name());
+        return this;
+    }
+    public HelseOgAndreHensynData setVerdi(HelseHinderSvar helseHinderSvar) {
+        verdi = helseHinderSvar;
+        return this;
+    }
 }
