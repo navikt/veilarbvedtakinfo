30bfecdb0b4dc42b3d92d2a360ef58eacb4dde72
hakon planke holm <hakon.planke.holm@nav.no>
2019-06-04 10:07:30 +0200

    [FO-2244] laggt lagt til config anotation

--- src/main/java/no/nav/fo/veilarbvedtakinfo/config/KafkaConfig.java
+++ src/main/java/no/nav/fo/veilarbvedtakinfo/config/KafkaConfig.java
@@ -6,6 +6,7 @@ import org.apache.kafka.clients.producer.ProducerConfig;
 import org.apache.kafka.common.config.SaslConfigs;
 import org.apache.kafka.common.serialization.StringSerializer;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.kafka.core.DefaultKafkaProducerFactory;
 import org.springframework.kafka.core.KafkaTemplate;
 import org.springframework.kafka.support.serializer.JsonSerializer;
@@ -15,6 +16,7 @@ import java.util.Properties;
 import static no.nav.sbl.util.EnvironmentUtils.getRequiredProperty;
 import static no.nav.sbl.util.EnvironmentUtils.requireEnvironmentName;
 
+@Configuration
 public class KafkaConfig {
 
     public static final String KAFKA_TOPIC = "privat-fo-vedtaksinfooppdatert-" + requireEnvironmentName();
