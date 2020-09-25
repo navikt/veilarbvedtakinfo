8c037956bb37066529fd8e7cc16993bfdbd96586
Sneha Desai <sneha.desai@nav.no>
2020-09-25 09:33:56 +0200

    FO-2947: Fjernet Produces annotation

--- src/main/java/no/nav/fo/veilarbvedtakinfo/controller/ArbeidsSituasjonResource.java
+++ src/main/java/no/nav/fo/veilarbvedtakinfo/controller/ArbeidsSituasjonResource.java
@@ -1,23 +1,19 @@
 package no.nav.fo.veilarbvedtakinfo.controller;
 
 import io.swagger.annotations.Api;
-
 import no.nav.common.abac.Pep;
+import no.nav.common.client.aktorregister.AktorregisterClient;
 import no.nav.fo.veilarbvedtakinfo.domain.arbeidSitasjon.ArbeidSituasjon;
 import no.nav.fo.veilarbvedtakinfo.domain.arbeidSitasjon.ArbeidSituasjonSvar;
 import no.nav.fo.veilarbvedtakinfo.service.ArbeidSitasjonService;
 import no.nav.fo.veilarbvedtakinfo.service.UserService;
-import no.nav.common.client.aktorregister.AktorregisterClient;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import javax.ws.rs.Produces;
-
 @RestController
 @RequestMapping("/api/situasjon")
-@Produces("application/json")
 @Api(value = "ArbeidsSituasjonResource")
 public class ArbeidsSituasjonResource {
     private final UserService userService;
