6e1fcef6c166d5a218fa0681f46f93d1600a749c
Alexander Gård <alex.gaard@hotmail.com>
2020-10-23 10:55:55 +0200

    Fjernet ubrukt kode for info om meg

--- src/main/java/no/nav/fo/veilarbvedtakinfo/domain/infoommeg/HovedmalSvar.java
+++
@@ -1,9 +0,0 @@
-package no.nav.fo.veilarbvedtakinfo.domain.infoommeg;
-
-public enum HovedmalSvar {
-    SAMME_ARBEIDSGIVER,
-    SAMME_ARBEIDSGIVER_NY_STILLING,
-    NY_ARBEIDSGIVER,
-    JOBBE_MER,
-    IKKE_OPPGITT
-}
