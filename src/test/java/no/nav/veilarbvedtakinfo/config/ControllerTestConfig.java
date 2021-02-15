fb1787afc70599ac93ae7a74d3b5dfbccebfd656
matiasvinjevoll <matias.vinjevoll@nav.no>
2021-02-15 15:07:18 +0100

    Fiks helsesjekk og metrikker - Legg til avhengighet til prometheus for å få autokonfigurert endepunkt - endepunkt for selftest - inkluder selftestresultat i ptometheus - fjern local profile

--- src/test/java/no/nav/veilarbvedtakinfo/config/ControllerTestConfig.java
+++ src/test/java/no/nav/veilarbvedtakinfo/config/ControllerTestConfig.java
@@ -2,6 +2,7 @@ package no.nav.veilarbvedtakinfo.config;
 
 import no.nav.veilarbvedtakinfo.controller.ArbeidsSituasjonController;
 import no.nav.veilarbvedtakinfo.controller.BehovsvurderingController;
+import no.nav.veilarbvedtakinfo.controller.InternalController;
 import no.nav.veilarbvedtakinfo.controller.MotestotteController;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Import;
@@ -11,5 +12,6 @@ import org.springframework.context.annotation.Import;
         ArbeidsSituasjonController.class,
         BehovsvurderingController.class,
         MotestotteController.class,
+        InternalController.class
 })
 public class ControllerTestConfig {}
