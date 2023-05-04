43415091f4daa60c961f98918fbd063017bdc523
sigurdgroneng <sigurd.groneng@nav.no>
2023-05-04 13:47:42 +0200

    Fixup use correct fnr param

--- src/main/java/no/nav/veilarbvedtakinfo/controller/MotestotteController.java
+++ src/main/java/no/nav/veilarbvedtakinfo/controller/MotestotteController.java
@@ -43,7 +43,7 @@ public class MotestotteController {
         Fnr brukerFnr = FnrUtils.hentFnrFraUrlEllerToken(authService, fnr);
         AktorId aktorId = authService.hentAktorId(brukerFnr);
 
-        authService.sjekkLeseTilgangTilPerson(fnr);
+        authService.sjekkLeseTilgangTilPerson(brukerFnr);
 
         Motestotte motestotte = msRepo.hentMoteStotte(aktorId);
 
