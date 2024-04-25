4a696e72eb501ba8aaab772608e0ad01c045f826
Klara M Helgemo <klara.margrethe.helgemo@nav.no>
2024-04-25 14:35:17 +0200

    Legg til bønne

--- src/main/java/no/nav/veilarbvedtakinfo/config/ApplicationConfig.java
+++ src/main/java/no/nav/veilarbvedtakinfo/config/ApplicationConfig.java
@@ -5,14 +5,21 @@ import no.nav.common.abac.VeilarbPepFactory;
 import no.nav.common.abac.audit.SpringAuditRequestInfoSupplier;
 import no.nav.common.auth.context.AuthContextHolder;
 import no.nav.common.auth.context.AuthContextHolderThreadLocal;
+import no.nav.common.client.aktoroppslag.AktorOppslagClient;
+import no.nav.common.client.aktoroppslag.CachedAktorOppslagClient;
+import no.nav.common.client.aktoroppslag.PdlAktorOppslagClient;
 import no.nav.common.token_client.builder.AzureAdTokenClientBuilder;
 import no.nav.common.token_client.client.AzureAdMachineToMachineTokenClient;
+import no.nav.common.token_client.client.MachineToMachineTokenClient;
 import no.nav.common.utils.Credentials;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
 
+import static no.nav.common.utils.EnvironmentUtils.isProduction;
 import static no.nav.common.utils.NaisUtils.getCredentials;
+import static no.nav.common.utils.UrlUtils.createServiceUrl;
 
 @Configuration
 @EnableConfigurationProperties({EnvironmentProperties.class})
@@ -38,4 +45,20 @@ public class ApplicationConfig {
         return AuthContextHolderThreadLocal.instance();
     }
 
+    @Bean
+    @Profile("!local")
+    public String pdlUrl() {
+        return createServiceUrl("pdl-api", "pdl", false);
+    }
+
+    @Bean
+    public AktorOppslagClient aktorClient(String pdlUrl, MachineToMachineTokenClient tokenClient) {
+        String tokenScope = String.format("api://%s-fss.pdl.pdl-api/.default",
+                isProduction().orElse(false) ? "prod" : "dev"
+        );
+        return new CachedAktorOppslagClient(new PdlAktorOppslagClient(
+                pdlUrl,
+                () -> tokenClient.createMachineToMachineToken(tokenScope))
+        );
+    }
 }
